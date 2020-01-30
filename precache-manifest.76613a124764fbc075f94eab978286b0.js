7fd01eed9c431dcdb0058a3588dd065831dc700e
unknown <lapwailau@gmail.com>
2020-01-30 19:22:27 +1300

    Updates

---
+++ precache-manifest.76613a124764fbc075f94eab978286b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e64cde0d6270ab6f392e00227b9d0a",
+    "url": "/react-axios/index.html"
+  },
+  {
+    "revision": "d13ee8ed94127cc06c67",
+    "url": "/react-axios/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "581ac79bc227c42320dd",
+    "url": "/react-axios/static/css/main.5a73ec31.chunk.css"
+  },
+  {
+    "revision": "d13ee8ed94127cc06c67",
+    "url": "/react-axios/static/js/2.81f6c74b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-axios/static/js/2.81f6c74b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "581ac79bc227c42320dd",
+    "url": "/react-axios/static/js/main.fa307775.chunk.js"
+  },
+  {
+    "revision": "3e98b9edcacfcbf42285",
+    "url": "/react-axios/static/js/runtime-main.c80be0cd.js"
+  }
+]);
